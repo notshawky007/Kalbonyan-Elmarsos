fc04b5aa8ed97b24be9a0828ccfe4362427617e4
shawky_53 <mostafashawky100000@gmail.com>
2023-08-13 03:41:35 +0300

    connect switch button

--- 02-Udemy/-02-JavaScript-Maximilian/Code/Section 12/practice-oop-01-starting-setup/assets/scripts/app.js
+++ 02-Udemy/-02-JavaScript-Maximilian/Code/Section 12/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,10 +1,17 @@
-// تعديل الشيفرة لتصحيح الأخطاء
-
 class Tooltip {}
 
 class ProjectItem {
   constructor(id) {
     this.id = id;
+    this.connectMoreInfoButton();
+  }
+
+  connectMoreInfoButton() {}
+
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click");
   }
 }
 
@@ -16,6 +23,13 @@ class ProjectList {
       this.projects.push(new ProjectItem(prjItem.id));
     }
     console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    this.addProject();
+    this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
 
